34d68834977d0174ccb83f45057bda339cc891e0
Evgeniy <45999443+Quintesence@users.noreply.github.com>
2024-10-16 15:32:50 +0300

    add favicon

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -4,7 +4,21 @@ import project01Big from "./../img/projects/01-big.png";
 import project02 from "./../img/projects/02.png";
 import project02Big from "./../img/projects/02-big.png";
 
+import project03 from "./../img/projects/03.png";
+import project03Big from "./../img/projects/03-big.png";
+
 const projects = [
+  {
+    title: "Ote! | FastFood Restaurant",
+    img: project02,
+    imgBig: project02Big,
+    skills: "HTML, CSS, ReactJS, Figma",
+    gitHubLink: "https://github.com",
+    behanceLink:
+      "https://www.behance.net/gallery/152299925/Ote-Kazakhstan-FastFood-Restaurant",
+    figmaLink:
+      "https://www.figma.com/design/wmhbuZZr6VeVWdsuseNBkD/Ote-Kazakhstan?node-id=0-1&node-type=canvas&t=OqzpGXdY13MM85xX-0",
+  },
   {
     title: "COLIN'S | Clothing Store",
     skills: "HTML, CSS, ReactJS, Figma",
@@ -17,15 +31,11 @@ const projects = [
       "https://www.figma.com/design/vdQGfKLGWupw4Cunjdnntp/COLIN’S-Kazakhstan?node-id=0-1&node-type=canvas&t=dHQpyWpdMoyYIsBS-0",
   },
   {
-    title: "Ote! | FastFood Restaurant",
-    img: project02,
-    imgBig: project02Big,
-    skills: "HTML, CSS, ReactJS, Figma",
-    gitHubLink: "https://github.com",
-    behanceLink:
-      "https://www.behance.net/gallery/152299925/Ote-Kazakhstan-FastFood-Restaurant",
-    figmaLink:
-      "https://www.figma.com/design/wmhbuZZr6VeVWdsuseNBkD/Ote-Kazakhstan?node-id=0-1&node-type=canvas&t=OqzpGXdY13MM85xX-0",
+    title: "Freelance Portfolio",
+    img: project03,
+    imgBig: project03Big,
+    skills: "HTML, CSS, ReactJS",
+    gitHubLink: "https://quintesence.github.io/freelance-portfolio/",
   },
 ];
 
