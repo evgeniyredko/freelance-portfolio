ab5294e67d3d9b39a883f825e05ffb8450319c50
Evgeniy <45999443+Quintesence@users.noreply.github.com>
2024-11-04 22:27:27 +0300

    add somthing

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -7,12 +7,22 @@ import project02Big from "./../img/projects/02-big.png";
 import project03 from "./../img/projects/03.png";
 import project03Big from "./../img/projects/03-big.png";
 
+import project04 from "./../img/projects/04.png";
+import project04Big from "./../img/projects/04-big.png";
+
+import project05 from "./../img/projects/05.png";
+import project05Big from "./../img/projects/05-big.png";
+
+import project06 from "./../img/projects/06.png";
+import project06Big from "./../img/projects/06-big.png";
+
 const projects = [
   {
     title: "Ote! | FastFood Restaurant",
     img: project02,
     imgBig: project02Big,
     skills: "HTML, CSS, ReactJS, Figma",
+    link: "#!",
     gitHubLink: "https://github.com",
     behanceLink:
       "https://www.behance.net/gallery/152299925/Ote-Kazakhstan-FastFood-Restaurant",
@@ -24,6 +34,7 @@ const projects = [
     skills: "HTML, CSS, ReactJS, Figma",
     img: project01,
     imgBig: project01Big,
+    link: "#!",
     gitHubLink: "https://github.com",
     behanceLink:
       "https://www.behance.net/gallery/151466779/COLINS-Kazakhstan-Clothing-Store",
@@ -31,11 +42,37 @@ const projects = [
       "https://www.figma.com/design/vdQGfKLGWupw4Cunjdnntp/COLIN’S-Kazakhstan?node-id=0-1&node-type=canvas&t=dHQpyWpdMoyYIsBS-0",
   },
   {
-    title: "Freelance Portfolio",
+    title: "Site | In Progress",
+    img: project06,
+    imgBig: project06Big,
+    skills: "HTML, CSS, ReactJS",
+    link: "#!",
+    gitHubLink: "#!",
+  },
+  {
+    title: "LoftHouse | Housing Complex",
+    img: project05,
+    imgBig: project05Big,
+    skills: "HTML, CSS, ReactJS",
+    link: "https://quintesence.github.io/loft-house/",
+    gitHubLink: "https://github.com/Quintesence/loft-house/tree/gh-pages",
+  },
+  {
+    title: "FASHION | Clothing Store",
+    img: project04,
+    imgBig: project04Big,
+    skills: "HTML, CSS, ReactJS",
+    link: "https://quintesence.github.io/fashion/",
+    gitHubLink: "https://github.com/Quintesence/fashion/tree/gh-pages",
+  },
+  {
+    title: "My Freelance Portfolio",
     img: project03,
     imgBig: project03Big,
     skills: "HTML, CSS, ReactJS",
-    gitHubLink: "https://quintesence.github.io/freelance-portfolio/",
+    link: "https://quintesence.github.io/freelance-portfolio/",
+    gitHubLink:
+      "https://github.com/Quintesence/freelance-portfolio/tree/gh-pages",
   },
 ];
 
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -13,13 +13,13 @@ const Project = () => {
       <div className="container">
         <div className="project-details">
           <h1 className="title-1">{project.title}</h1>
-
-          <img
-            src={project.imgBig}
-            alt={project.title}
-            className="project-details__cover"
-          />
-
+          <a href={project.link} target="_blank" rel="noreferrer">
+            <img
+              src={project.imgBig}
+              alt={project.title}
+              className="project-details__cover"
+            />
+          </a>
           <div className="project-details__desc">
             <p>Skills: {project.skills}</p>
           </div>
